1a86bd739b691632f653ea6cfcf9989d893e030c
Jill Cates <cates.jill@gmail.com>
2019-11-18 22:38:21 -0500

    Update doc generation script

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,14 +1,13 @@
 echo "Copying files over..."
 cp ../notebooks/walkthrough.ipynb source/notebooks/walkthrough.ipynb
-cp ../markdown/getting_started.md source/markdown/getting_started.md
+cp -r ../markdown/ source/markdown/
 
 echo "Generating docs..."
 make html
 
 echo "Cleaning up..."
 rm source/notebooks/walkthrough.ipynb
-rm source/markdown/getting_started.md
-
+rm source/markdown/
 
 #! echo "Copying docs to doc folder..."
 #! cp -r build/html/ ../../tutorial-docs/
